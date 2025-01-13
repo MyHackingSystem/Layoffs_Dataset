b807c65279fb0b108c331fb6488909427d2a95e5
rawaha-coder <66226622+rawaha-coder@users.noreply.github.com>
2025-01-13 14:32:46 +0100

    Add files via upload

--- Cleaning_And_Exploring_World_Layoffs_Dataset.sql
+++ Cleaning_And_Exploring_World_Layoffs_Dataset.sql
@@ -1,5 +1,9 @@
 -- @Zakariae el kadiri
--- "Layoffs Dataset Analytics" project, I focused on transforming raw data into actionable insights by applying data cleaning and exploration techniques. This project highlights my ability to work with real-world datasets, ensuring data quality and consistency for accurate analysis.
+
+-- "Layoffs Dataset Analytics" project, I focused on transforming raw data into actionable insights by applying data cleaning 
+-- and exploration techniques. This project highlights my ability to work with real-world datasets, ensuring data quality and 
+-- consistency for accurate analysis.
+
 -- Dataset source: https://www.kaggle.com/datasets/theakhilb/layoffs-data-2022
 
 -- Data Cleaning
@@ -473,23 +477,3 @@ FROM Company_Year
 SELECT * 
 FROM Company_Rank_Year
 WHERE Ranking <= 5;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
